b4e5251d51c4a29b503ce6dc97f4d062d2957f90
Mkalleby <marcoskalleby035@gmail.com>
2025-02-04 14:16:13 +0000

    aventureiro

--- xadrez.c
+++ xadrez.c
@@ -11,6 +11,8 @@ int main() {
     int bispo = 1;
     int torre = 1;
     int rainha = 1;
+    int cavalo_vertical;
+    int cavalo_horizontal;
     int selecao;
 
     do
@@ -22,6 +24,7 @@ int main() {
         printf("1- Bispo\n");
         printf("2- Torre\n");
         printf("3- Rainha\n");
+        printf("4- Cavalo\n");
         printf("\n");
         printf("Selecione a peça que você deseja movimentar: ");
         scanf("%d", &selecao);
@@ -39,12 +42,12 @@ int main() {
             printf("Você poderá movimentar o bispo em até 5 casas para a diagonal.\n");
             printf("\n");
             while (bispo <= 5){
-                printf("Movimentando %d casa(as) para direita e %d para cima(diagonal).\n", bispo, bispo);
+                printf("Movimentando a %dª casa para direita e a %dª casa para cima(diagonal).\n", bispo, bispo);
                 printf("%d / 5 casas movidas para a diagonal.\n", bispo);
-                printf("Total de casas movidas: %d\n", bispo + bispo);
                 printf("\n");
                 bispo++;
             }
+            printf("Total de casas movidas: %d\n", bispo + bispo);
             printf("----------Fim do programa----------\n");
         break;
 
@@ -59,7 +62,7 @@ int main() {
             printf("\n");
             while (torre <= 5)
             {
-                printf("Movimentando %d casa(as) para direita.\n", torre);
+                printf("Movimentando %dª casa para direita.\n", torre);
                 printf("%d / 5 casas movidas para a direita.\n", torre);
                 printf("\n");
                 torre++;
@@ -78,11 +81,44 @@ int main() {
             printf("\n");
             while (rainha <= 8)
             {
-                printf("Movimentando %d casa(as) para esquerda.\n", rainha);
+                printf("Movimentando %dª casa para esquerda.\n", rainha);
                 printf("%d / 8 casas movidas para a esquerda.\n", rainha);
                 printf("\n");
                 rainha++;
             }
+        break;
+
+        // Nível Aventureiro - Movimentação do Cavalo
+        case 4: 
+            // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
+            // Um loop pode representar a movimentação horizontal e outro vertical.
+            printf("---Cavalo--- \n");
+            printf("\n");
+            printf("*Você está movimentando o cavalo*\n");
+            printf("Você poderá movimentar cavalo em formato de L.\n");
+            printf("\n");
+            for (cavalo_vertical = 1; cavalo_vertical < 2; cavalo_vertical++)
+            {
+                printf("*Horizontal*\n");
+                printf("\n");
+                for (cavalo_horizontal = 0; cavalo_horizontal < 2; cavalo_horizontal++)
+                {
+                   printf("Movimentando %dª casa para a horizontal (em L).\n", cavalo_horizontal + 1);
+                   printf("%d / 2 casas movidas para a horizontal.\n", cavalo_horizontal + 1);
+                }
+                printf("\n");
+                printf("*Vertical*\n");
+                printf("Movimentando %dª casa para a vertical (em L).\n", cavalo_vertical);
+                printf("%d / 1 casas movidas para a vertical.\n", cavalo_vertical);
+                printf("\n");
+                printf("Total de casas movidas: %d", cavalo_vertical + cavalo_horizontal);
+                printf("\n");
+            }
+            
+
+
+
+        break;
             printf("----------Fim do programa----------\n");
         break;
         
@@ -91,13 +127,9 @@ int main() {
             printf("\n");
         break;
         }
-    } while (selecao < 1 || selecao > 3);
+    } while (selecao < 1 || selecao > 4);
     //verificando o o número digitado correspondem aos cases do switch
     
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
@@ -108,3 +140,4 @@ int main() {
 
     return 0;
 }
+
